645dc9b605b958c8ad85e1fabbf953a1637a92d5
Cristian Melo <cristianmeelo@gmail.com>
2023-02-06 20:20:31 -0300

    :sparkles: feat: implemented components Home & Contact

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+// ~ Dependencies
+import React  from 'react'
 import ReactDOM from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+// ~ Components
+import { Home } from './routes/Home'
+import { Contact } from './routes/Contact'
 import App from './App'
+
+// ~ Design
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      { path: "/", element: <Home />},
+      { path: "/contact", element: <Contact />},
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
