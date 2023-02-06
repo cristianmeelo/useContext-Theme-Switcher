c49b2b2952af263c749f3d6342e22ae73103679b
Cristian Melo <cristianmeelo@gmail.com>
2023-02-06 20:23:37 -0300

    :sparkles: feat: implemented component Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import { Outlet } from "react-router";
 
 // ~ Design
 import "./App.css";
+import { Navbar } from './components/Navbar';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
+      <Navbar/>
       <Outlet />
     </div>
   );
