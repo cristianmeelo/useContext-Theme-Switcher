52faff4b242f38b2aab9460a05cfacf12fe0d89a
Cristian Melo <cristianmeelo@gmail.com>
2023-02-07 23:54:29 -0300

    :sparkles: feat: implemented Footer component

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -4,17 +4,21 @@ import React, { useContext } from 'react'
 // ~Context
 import { ThemeContext } from '../context/ThemeContext';
 
+// ~Component
+import { Footer } from '../components/Footer';
+
 export const Contact = () => {
   const { theme, toggleTheme } = useContext(ThemeContext);
 
   return (
-    <div>
+    <div className='page-information-container'>
       <h1>Páginal de Contato</h1>
       <br/>
       <button onClick={toggleTheme}>Mudar tema</button>
       <br/>
       <br/>
       <p>O tema atual é: {theme}</p>
+      <Footer/>
     </div>
   )
 }
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -4,17 +4,21 @@ import React, { useContext } from "react";
 // ~ Contexts
 import { ThemeContext } from "../context/ThemeContext";
 
+// ~ Components
+import { Footer } from "../components/Footer";
+
 export const Home = () => {
   const { theme, toggleTheme } = useContext(ThemeContext);
 
   return (
-    <div>
+    <div className='page-information-container'>
       <h1>Páginal Inicial</h1>
       <br/>
       <button onClick={toggleTheme}>Mudar tema</button>
       <br/>
       <br/>
       <p>O tema atual é: {theme}</p>
+      <Footer/>
     </div>
   );
 };
